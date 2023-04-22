07f4ec4eeb9387f5ff7fae94ee3c455baf936fb7
XaxisPlayz <66885524+XaxisPlayz@users.noreply.github.com>
2023-04-22 14:33:51 -0700

    e

---
+++ src/main/java/io/github/xaxisplayz/Main.java
@@ -0,0 +1,18 @@
+package io.github.xaxisplayz;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+
+    @Override
+    public void onDisable() {
+
+
+    }
+
+    @Override
+    public void onEnable() {
+
+
+    }
+}
