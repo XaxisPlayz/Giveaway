a487fba333cad9ca5107a87d55b82e8681f948ea
XaxisPlayz <66885524+XaxisPlayz@users.noreply.github.com>
2023-04-24 12:47:32 -0700

    readability

--- src/main/java/io/github/xaxisplayz/Main.java
+++ src/main/java/io/github/xaxisplayz/Main.java
@@ -1,8 +1,8 @@
 package io.github.xaxisplayz;
 
-import io.github.xaxisplayz.commands.GiveawayItems;
-import io.github.xaxisplayz.commands.JoinGiveaway;
-import io.github.xaxisplayz.commands.ManualGiveaway;
+import io.github.xaxisplayz.commands.GiveawayItemsCommand;
+import io.github.xaxisplayz.commands.JoinGiveawayCommand;
+import io.github.xaxisplayz.commands.ManualGiveawayCommand;
 import io.github.xaxisplayz.manager.GiveawayManager;
 import net.kyori.adventure.text.Component;
 import org.bukkit.Bukkit;
@@ -13,77 +13,66 @@ import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitTask;
 
-import java.util.List;
-import java.util.UUID;
-
-
-@SuppressWarnings("deprecation")
 public class Main extends JavaPlugin {
 
-    public static Component GUI_TITLE = Component.text(colorize("&aGiveaway Items!"));
-
-    public GiveawayManager giveawayManager;
+    private static final Component GUI_TITLE = Component.text(ChatColor.GREEN + "Giveaway Items!");
 
     private BukkitTask task;
-
-    /*
-    - Every 4 hours a giveaway starts in chat
-    - Clickable Message to join
-    - 20s giveaway starts, random winner chosen
-    - if inv full choose different player
-    - Run a command that opens a gui to place items in it, an item will be randomly chosen for the giveaway.
-    - hold an item and run a giveaway with the item in hand for manual giveaway
-     */
-
-    public static String colorize(String s){
-        return ChatColor.translateAlternateColorCodes('&', s);
-    }
+    private GiveawayManager giveawayManager;
 
     @Override
     public void onEnable() {
-
-        new GiveawayItems(this);
-        new onInvClose(this);
-        new JoinGiveaway(this);
-        new ManualGiveaway(this);
+        getCommand("giveawayitems").setExecutor(new GiveawayItemsCommand(this));
+        getCommand("joingiveaway").setExecutor(new JoinGiveawayCommand(this));
+        getCommand("manualgiveaway").setExecutor(new ManualGiveawayCommand(this));
 
         saveDefaultConfig();
-        Bukkit.getScheduler().runTaskLater(this, this::runTask,4L*60L*60L*1000L);
-        if(getConfig().get("Count") == null){
-            getConfig().set("Count",0);
+        long delay = 4L * 60L * 60L * 20L; // 4 hours
+        Bukkit.getScheduler().runTaskLater(this, this::startGiveawayTask, delay);
+
+        if(getConfig().get("Count") == null) {
+            getConfig().set("Count", 0);
             saveConfig();
         }
     }
 
-    public BukkitTask getTask() {
-        return task;
+    private void startGiveawayTask() {
+        giveawayManager = new GiveawayManager(this);
+        task = Bukkit.getScheduler().runTaskTimer(this, giveawayManager, 0L, delay);
+    }
+
+    public void setConfigItems(Inventory inventory) {
+        int count = getConfig().getInt("Count");
+        for(ItemStack item : inventory.getContents()) {
+            if(item != null && item.getType() != Material.AIR) {
+                getConfig().set(String.format("'%d'", count), item);
+                count++;
+            }
+        }
+        getConfig().set("Count", count);
+        saveConfig();
+    }
+
+    @Override
+    public void onDisable() {
+        if (task != null) {
+            task.cancel();
+        }
+    }
+    
+    public static String colorize(String s) {
+        return ChatColor.translateAlternateColorCodes('&', s);
     }
 
     public GiveawayManager getGiveawayManager() {
         return giveawayManager;
     }
 
-    public void runTask(){
-        task = getServer().getScheduler().runTaskTimer(this, ()->{
-            new GiveawayManager(this);
-        }, 0L, 4L * 60L * 60L * 1000L);
+    public Component getGuiTitle() {
+        return GUI_TITLE;
     }
 
-    public void setConfigItems(Inventory inventory){
-        for(ItemStack item : inventory.getContents()){
-            if(item == null) continue;
-            if(item.getType() == Material.AIR) continue;
-            getConfig().set("'" + getConfig().getInt("Count") + "'",item);
-            getConfig().set("Count",getConfig().getInt("Count") + 1);
-        }
-        saveConfig();
+    public BukkitTask getTask() {
+        return task;
     }
-
-    @Override
-    public void onDisable() {
-    }
-
-
-
-
 }
