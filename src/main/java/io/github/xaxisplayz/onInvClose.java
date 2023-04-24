e693f3fbf24de3720c07f321287526777c08ec80
XaxisPlayz <66885524+XaxisPlayz@users.noreply.github.com>
2023-04-24 12:43:06 -0700

    readability

--- src/main/java/io/github/xaxisplayz/onInvClose.java
+++ src/main/java/io/github/xaxisplayz/onInvClose.java
@@ -1,7 +1,3 @@
-package io.github.xaxisplayz;
-
-import net.kyori.adventure.text.Component;
-import org.bukkit.Material;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryCloseEvent;
@@ -16,24 +12,42 @@ public class onInvClose implements Listener {
 
     public onInvClose(Main plugin){
         this.plugin = plugin;
-        plugin.getServer().getPluginManager().registerEvents(this,plugin);
+        plugin.getServer().getPluginManager().registerEvents(this, plugin);
     }
 
     @EventHandler
     public void onPlayerInvClose(InventoryCloseEvent event){
         Inventory inv = event.getInventory();
         Component title = event.getView().title();
-        if(!title.equals(Main.GUI_TITLE) && event.getPlayer().hasPermission("giveaway.admin")) return;
-        if(inv.isEmpty()) return;
-        if(plugin.getGiveawayManager() == null) {
-            plugin.setConfigItems(inv);
-            event.getPlayer().sendMessage(Main.colorize("&aSuccessfully set items!"));
+
+        if (!isPlayerAuthorized(event.getPlayer())) {
             return;
         }
-        plugin.getGiveawayManager().getItems().clear();
-        plugin.setConfigItems(inv);
-        plugin.getGiveawayManager().addItems(inv);
-        event.getPlayer().sendMessage(Main.colorize("&aSuccessfully set items!"));
 
+        if (!Main.GUI_TITLE.equals(title)) {
+            return;
+        }
+
+        if (isEmptyInventory(inv)) {
+            return;
+        }
+
+        if (plugin.getGiveawayManager() == null) {
+            plugin.setConfigItems(inv);
+        } else {
+            plugin.getGiveawayManager().getItems().clear();
+            plugin.setConfigItems(inv);
+            plugin.getGiveawayManager().addItems(inv);
+        }
+
+        event.getPlayer().sendMessage(Main.colorize("&aSuccessfully set items!"));
+    }
+
+    private boolean isPlayerAuthorized(Player player) {
+        return player.hasPermission("giveaway.admin");
+    }
+
+    private boolean isEmptyInventory(Inventory inventory) {
+        return inventory.isEmpty();
     }
 }
